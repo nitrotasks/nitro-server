b48f62102ee31f9535ed4da5c4472564ac031843
George Czabania <dev@stayradiated.com>
2012-04-08 12:39:56 +1200

    Export tasks to object

--- scripts.js
+++ scripts.js
@@ -3,5 +3,15 @@ $(document).ready(function() {
 	$('.add').click(function() {
 		$('.main').append('<li class="' + id + '">' + $('input').val() + '</li>');
 		$('input').val('');
+
+		save();
 	});
+
+	function save() {
+		var tasks = {};
+		$('.main li').each(function(index, key) {
+			tasks[index] = $(key).html();
+		});
+		console.log(tasks);
+	}
 });
