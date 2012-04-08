f86e0a60a3230b3ec723dc678c7820aabd049449
George Czabania <dev@stayradiated.com>
2012-04-08 12:36:16 +1200

    Basic list manager

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
 $(document).ready(function() {
-
+	var id = 0;
+	$('.add').click(function() {
+		$('.main').append('<li class="' + id + '">' + $('input').val() + '</li>');
+		$('input').val('');
+	});
 });
