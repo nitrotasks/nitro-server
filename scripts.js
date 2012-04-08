f10dd1bd4741e475c122adbe098d924036902c31
George Czabania <dev@stayradiated.com>
2012-04-08 13:59:00 +1200

    Got rid of adding tasks code

--- scripts.js
+++ scripts.js
@@ -1,33 +1,3 @@
-$(document).ready(function() {
-
-	var id = 0;
-	if($.jStorage.get('tasks')) {
-		for (var key in $.jStorage.get('tasks')) {
-			add($.jStorage.get('tasks')[key], key);
-		}
-	}
-
-	$('.add').click(function() {
-		add($('input').val());
-		$('input').val('');
-		save();
-	});
-
-	function add(value, _id) {
-		if(!_id) _id = id;
-		$('.main').append('<li class="' + _id + '">' + value + '</li>');
-		id++;
-	}
-
-	function save() {
-		var tasks = {};
-		$('.main li').each(function(index, key) {
-			tasks[$(key).attr('class')] = $(key).html();
-		});
-		$.jStorage.set('tasks', tasks);
-	}
-});
-
 function get(computer) {
 	server = new Object()
 	comp = new Object()
