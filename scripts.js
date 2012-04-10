e3253875cf59cc7155a67709fb1fd27978c0a75e
George Czabania <dev@stayradiated.com>
2012-04-10 14:30:22 +1200

    Handles task that have been deleted on either the server or the computer

--- scripts.js
+++ scripts.js
@@ -13,6 +13,9 @@ server = {
 		content: { value: "The third task", time: 0 },
 		notes: { value:"", time: 0 },
 		priority: { value:"none", time: 0 }
+	},
+	4: {
+		deleted: 1
 	}
 }
 
@@ -25,7 +28,15 @@ $(document).ready(function() {
 });
 
 function add(task, id, device) {
-	$('#' + device + ' ul').append('<li class="' + id + '" data-priority="' + task.priority.value + '" data-time="' + task.content.time + '|' + task.notes.time + '|' + task.priority.time + '"><span class="content">' + task.content.value + '</span><span class="notes">' + task.notes.value + '</span></li>');
+
+	// If task is deleted, show greyed out task
+	if(task.hasOwnProperty('deleted')) {
+		$('#' + device + ' ul').append('<li class="' + id + '" data-deleted="true" data-time="' + task.deleted + '"><span class="content">Deleted</span></li>');
+
+	// Else show task as normal
+	} else {
+		$('#' + device + ' ul').append('<li class="' + id + '" data-priority="' + task.priority.value + '" data-time="' + task.content.time + '|' + task.notes.time + '|' + task.priority.time + '"><span class="content">' + task.content.value + '</span><span class="notes">' + task.notes.value + '</span></li>');
+	}
 }
 
 function clone(input) {
@@ -46,33 +57,79 @@ function get(computer) {
 		for(var x in time) {
 			time[x] = parseInt(time[x]);
 		}
+		
+		if($(this).attr('data-deleted') == 'true') {
 
-		// Create computer
-		comp[$(this).attr('class')] = {
-			content:  {
-				value: $(this).find('.content').text(),
-				time: time[0]
-			},
-			notes: {
-				value: $(this).find('.notes').text(),
-				time: time[1]
-			},
-			priority: {
-				value: $(this).attr('data-priority'),
-				time: time[2]
+			// Task has been deleted!
+			comp[$(this).attr('class')] = {
+				deleted: time[0]
+			}
+
+		} else {
+
+			// Get attributes
+			comp[$(this).attr('class')] = {
+				content:  {
+					value: $(this).find('.content').text(),
+					time: time[0]
+				},
+				notes: {
+					value: $(this).find('.notes').text(),
+					time: time[1]
+				},
+				priority: {
+					value: $(this).attr('data-priority'),
+					time: time[2]
+				}
 			}
 		}
-		console.log(comp);
 	});
 
+	// Loop through each task
 	for(var task in comp) {
-		for(var key in comp[task]) {
-			if(comp[task][key].time > server[task][key].time) {
-				server[task][key] = clone(comp[task][key]);
+
+		// Task was deleted on computer
+		if(comp[task].hasOwnProperty('deleted')) {
+
+			var deleteTask = true;
+
+			// Loop through each attribute on server
+			for(var key in server[task]) {
+				// Check if server task was modified after task was deleted
+				if(server[task][key].time > comp[task].deleted) {
+
+					deleteTask = false;
+
+				}
+			}
+
+			if(deleteTask) {
+				server[task] = clone(comp[task]);
+			}
+
+		} else {
+			// Loop through each attribute on computer
+			for(var key in comp[task]) {
+				// Check if task was deleted on server
+				 if (server[task].hasOwnProperty('deleted')) {
+					// Check if task was modified after it was deleted
+					if(comp[task][key].time > server[task].deleted) {
+						// Update the server with the non-deleted version
+						server[task] = clone(comp[task]);
+					}
+				// Task has not been deleted
+				} else {
+					// If the attribute was updated after the server
+					if(comp[task][key].time > server[task][key].time) {
+						// Update the servers version
+						server[task][key] = clone(comp[task][key]);
+					}
+				}		
 			}
 		}
 	}
 
+	// Print to UI
 	$('#server ul').html('');
 	$('#' + computer + ' ul').html('');
 	for(var task in server) {
