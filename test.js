2c26b147f4a20d80502bad63c2ab584742c2d7ae
George Czabania <dev@stayradiated.com>
2012-05-13 13:56:26 +1200

    Google API with OAuth2 - doesn't work yet

--- test.js
+++ test.js
@@ -1,50 +1,10 @@
-var OAuth = require("oauth").OAuth,
+var OAuth2 = require("oauth").OAuth2,
 	express = require("express").createServer();
 
-var oa = new OAuth("https://one.ubuntu.com/oauth/request/",
-					 "https://one.ubuntu.com/oauth/access/",
-					 "ubuntuone",
-					 "hammertime",
-					 "1.0",
-					 "http://localhost:3000",
-					 "PLAINTEXT")
+var scopes = "https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile";
 
-// var request = {
-// 	token: "",
-// 	secret: ""
-// }
+var oa = new OAuth2("208808167997.apps.googleusercontent.com", "bWYNj1RjRYagGHHrStugd1Ps", "https://www.googleapis.com", "/auth/drive", "/auth/drive")
 
-var access = {
-	token: "p05x2WVPTGj15J8vT9K3",
-	secret: "l9KjpNXfb73vf6qJKthqm3hgpwh0fDMcT2n1FcTN0D8CLx5kmHtbWC4nvxzmJhZjDnk8L6872vSXDR7Z"
-}
-
-oa.get("https://files.one.ubuntu.com/content/~/Ubuntu%20One/Nitro/super.json", access.token, access.secret, function (e, d, r) {
-	console.log(e ? e :  d);
-});
-
-// oa.getOAuthRequestToken(function(error, oauth_token, oauth_token_secret,  results){
-
-// 	if (error) return console.log('error :' + JSON.stringify(error))
-
-// 	request.token = oauth_token;
-// 	request.secret = oauth_token_secret
-
-// 	console.log('https://one.ubuntu.com/oauth/authorize/?oauth_token=' + oauth_token);
-// });
-
-// express.get('/', function (req, res) {
-// 	res.send("Hello World :D");
-// 	console.log("Requesting access token")
-// 	oa.getOAuthAccessToken(request.token, request.secret, req.query.oauth_verifier, function(error, oauth_access_token, oauth_access_token_secret, results) {
-// 		console.log('oauth_access_token :' + oauth_access_token)
-// 		console.log('oauth_token_secret :' + oauth_access_token_secret)
-// 		console.log("Requesting account details")
-// 		var data= "";
-// 		oa.getProtectedResource("https://one.ubuntu.com/api/file_storage/v1", "GET", oauth_access_token, oauth_access_token_secret, function (error, data, response) {
-// 			console.log(data);
-// 		});
-// 	});
-// });
-
-// express.listen(3000)
+oa.getOAuthAccessToken("", {scope: scopes, redirect_uri: "https://stark-fog-5496.herokuapp.com/googledrive"}, function(error, access_token, refresh_token, results) {
+	console.log(error, access_token, refresh_token, results);
+})
