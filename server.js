bc9afa0314df8967faacfdf53ae7de0e842ac4e1
George Czabania <george@czabania.com>
2014-01-29 13:41:08 +1300

    Support for Coffee Script 1.7.0

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 /*
- * Windows Azure Setup
+ * Start Nitro Server
  */
 
 global.DEBUG = true;
 
-require('coffee-script');
-require('./app/init.coffee');
+// Include the CoffeeScript interpreter so that .coffee files will work
+var coffee = require('coffee-script');
 
+// Explicitly register the compiler if required. This became necessary in CS 1.7
+if (typeof coffee.register !== 'undefined') coffee.register();
+
+// Include our application file
+var app = require('./app/init.coffee');
