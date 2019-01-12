f2a095735dc129db345ab6ed72ff54ddf846a997
Jono Cooper <jono@jonocooper.com>
2019-01-13 12:57:00 +1300

    Add Rudimentary Stats Endpoint Protection

--- config/config.js
+++ config/config.js
@@ -6,26 +6,33 @@ const config = {
   // dist: path.resolve(__dirname, '../../nitro/dist'),
 
   development: {
-    url: process.env.DATABASE_URL || 'postgres://nitro:secret@localhost:5432/nitro',
+    url:
+      process.env.DATABASE_URL ||
+      'postgres://nitro:secret@localhost:5432/nitro',
     dialect: 'postgres'
   },
   production: {
-    url: process.env.DATABASE_URL || 'postgres://nitro:secret@localhost:5432/nitro',
+    url:
+      process.env.DATABASE_URL ||
+      'postgres://nitro:secret@localhost:5432/nitro',
     dialect: 'postgres'
   },
   test: {
-    url: process.env.DATABASE_URL_TEST || 'postgres://nitro:secret@localhost:5432/nitrotest',
+    url:
+      process.env.DATABASE_URL_TEST ||
+      'postgres://postgres:@127.0.0.1/nitrotest',
     dialect: 'postgres'
   },
   travis: {
-    url: 'postgres://postgres:@127.0.0.1/nitro_travis',
+    url: 'postgres://postgres:@127.0.0.1/nitrotest',
     dialect: 'postgres'
   },
 
   jwtstrategy: process.env.JWT_Strategy || 'bearer',
   jwtaudience: process.env.JWT_Audience || 'https://uat.nitrotasks.com/a/',
   jwtissuer: process.env.JWT_Issuer || 'https://dymajo.au.auth0.com/',
-  jwksuri: process.env.JWKS_Uri || 'https://dymajo.au.auth0.com/.well-known/jwks.json',
+  jwksuri:
+    process.env.JWKS_Uri || 'https://dymajo.au.auth0.com/.well-known/jwks.json',
   jwtsecret: process.env.JWT_Secret || 'secret'
 }
 module.exports = config
